47c228e0b7d2c724aa989c6f54980d4aae28a542
Amith-B <amithbr6@gmail.com>
2022-05-02 23:15:05 +0530

    feat: update selectionsort algorithm logic

--- src/algorithms/selectionSort.js
+++ src/algorithms/selectionSort.js
@@ -1,17 +1,21 @@
 function selectionSort(inputArr, swapList) {
-  // TODO: replace below code with actual algorithm, below code is bubble sort
-  let len = inputArr.length;
-  let checked;
-  do {
-    checked = false;
-    for (let i = 0; i < len; i++) {
-      if (inputArr[i] > inputArr[i + 1]) {
-        [inputArr[i], inputArr[i + 1]] = [inputArr[i + 1], inputArr[i]];
-        swapList.push([i, i + 1]);
-        checked = true;
+  let n = inputArr.length;
+
+  for (let i = 0; i < n; i++) {
+    // Finding the smallest number in the subarray
+    let min = i;
+    for (let j = i + 1; j < n; j++) {
+      if (inputArr[j] < inputArr[min]) {
+        min = j;
       }
     }
-  } while (checked);
+    if (min != i) {
+      // Swapping the elements
+      [inputArr[i], inputArr[min]] = [inputArr[min], inputArr[i]];
+      swapList.push([i, min]);
+    }
+  }
+  return inputArr;
 }
 
 export default function sort(inputArr) {
