5121f54cd5b472fe57b6282f937fa8ba963dd2d7
Amith-B <amithbr6@gmail.com>
2022-05-02 23:31:14 +0530

    fix: remove radix sort as it is not swapable sort

--- src/algorithms/index.js
+++ src/algorithms/index.js
@@ -2,12 +2,10 @@ import bubble_sort from "./bubbleSort";
 import selection_sort from "./selectionSort";
 import quick_sort from "./quickSort";
 import insertion_sort from "./insertionSort";
-import radix_sort from "./radixSort";
 
 export default {
   bubble_sort,
   selection_sort,
   quick_sort,
   insertion_sort,
-  radix_sort,
 };
