4c25a889e424e54860800b372fbd3038a373c628
Amith-B <amithbr6@gmail.com>
2022-05-08 19:20:16 +0530

    feat: added github link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,10 +2,11 @@ import styles from "./Header.module.css";
 import { createSignal, createEffect } from "solid-js";
 import Modal from "./../Modal/Modal";
 import controls from "../../store/createControl";
+import githubLogo from "../../assets/github.svg";
 
 function Header() {
   const [showModal, setShowModal] = createSignal(false);
-  const { setPlaying } = controls;
+  const { setPlaying, themeTextColor } = controls;
 
   const handleModalToggle = () => {
     setShowModal((show) => !show);
@@ -22,13 +23,30 @@ function Header() {
       <Modal open={showModal()} triggerClose={handleModalToggle} />
       <header class={styles.Header}>
         <h3 class="theme-text__color">AlgoVisualizer</h3>
-        <button
-          class="controls theme-text__color"
-          onClick={handleModalToggle}
-          title="Settings"
-        >
-          <span class="material-symbols-outlined">settings</span>
-        </button>
+        <div class={styles.BtnGrp}>
+          <a
+            href="https://github.com/Amith-B/AlgoVisualizer"
+            target="_blank"
+            style={{ display: "flex" }}
+            title="Amith.B Github"
+          >
+            <img
+              alt="github"
+              src={githubLogo}
+              classList={{
+                [styles.GitHubLink]: true,
+                [styles.GitHubLinkInvert]: themeTextColor() === "white",
+              }}
+            />
+          </a>
+          <button
+            class="controls theme-text__color"
+            onClick={handleModalToggle}
+            title="Settings"
+          >
+            <span class="material-symbols-outlined">settings</span>
+          </button>
+        </div>
       </header>
     </>
   );
